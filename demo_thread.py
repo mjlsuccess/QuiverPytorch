ebe7b768e41c4f0fc00a9c3fb74a12edbe3ba822
mjlsuccess <1018584208@qq.com>
2020-09-15 18:06:05 +0800

    add model switch

--- demo_thread.py
+++ demo_thread.py
@@ -14,6 +14,22 @@ if __name__ == "__main__":
     thread.start()
     # thread.join() #block untile thread finish
 
+    while True:
+        a = input("input:")
+
+        if a == '0':
+            break
+        elif a == '1':
+            print("resnet")
+            model = models.resnet18(pretrained=False)
+            hook_list = register_hook(model) 
+            server.update_model(model, hook_list, "./data/Dog", [200,200])
+        elif a == '2':
+            print("vgg19")
+            model = models.vgg19(pretrained=False)
+            hook_list = register_hook(model) 
+            server.update_model(model, hook_list, "./data/Cat", [200,200])
+
     print("do other things below")
     for i in range(5):
         print(i)
--- quiver_engine/server.py
+++ quiver_engine/server.py
@@ -31,96 +31,72 @@ from quiver_engine.file_utils import list_img_files, save_layer_img
 from quiver_engine.vis_utils import save_layer_outputs
 
 
-def get_app(model, hooks, classes, top, html_base_dir, use_gpu, image_size, temp_folder='./tmp', input_folder='./',
-            mean=None, std=None):
-    '''
-    The base of the Flask application to be run
-    :param model: the model to show
-    :param classes: list of names of output classes to show in the GUI.
-        if None passed - ImageNet classes will be used
-    :param top: number of top predictions to show in the GUI
-    :param html_base_dir: the directory for the HTML (usually inside the
-        packages, quiverboard/dist must be a subdirectory)
-    :param temp_folder: where the temporary image data should be saved
-    :param input_folder: the image directory for the raw data
-    :param mean: list of float mean values
-    :param std: lost of float std values
-    :return:
-    '''
+app = Flask(__name__)
+app.threaded = True
+CORS(app)
 
-    # single_input_shape, input_channels = get_input_config(model)
-    app = Flask(__name__)
-    app.threaded = True
-    CORS(app)
+_json_graph = None
+_model = None
+_hook_list = None
 
+_html_base_dir = None
+_temp_folder = None
+_input_folder = None
+
+_use_gpu = False
+_image_size = [224,224]
+
+
+def register_routes():
+
+    global _json_graph, _model, _hook_list, _html_base_dir, _temp_folder, _input_folder, _use_gpu, _image_size
     '''
-        prepare model
-    '''
-    width = 224
-    height = 224
-    if image_size is not None:
-        width = image_size[-1]
-        height = image_size[-2]
-    x = torch.zeros(1, 3, width, height, dtype=torch.float, requires_grad=False)
-    
-    if use_gpu and torch.cuda.is_available():
-        x = x.cuda()
-        model = model.cuda()
-
-    out = model(x)
-
-    graph = make_dot(out, params = dict(model.named_parameters()))
-
-    '''
-        Static Routes
+        Static Routes: only call once
     '''
     @app.route('/')
     def home():
         return send_from_directory(
-            join(html_base_dir, 'quiverboard/dist'),
+            join(_html_base_dir, 'quiverboard/dist'),
             'index.html'
         )
 
     @app.route('/<path>')
     def get_board_files(path):
         return send_from_directory(
-            join(html_base_dir, 'quiverboard/dist'),
+            join(_html_base_dir, 'quiverboard/dist'),
             path
         )
 
     @app.route('/temp-file/<path>')
     def get_temp_file(path):
-        return send_from_directory(abspath(temp_folder), path)
+        return send_from_directory(abspath(_temp_folder), path)
 
     @app.route('/input-file/<path>')
     def get_input_file(path):
-        return send_from_directory(abspath(input_folder), path)
+        print("--------->",abspath(_input_folder), path)
+        return send_from_directory(abspath(_input_folder), path)
 
     '''
         Computations
     '''
     @app.route('/model')
     def get_config():
-        # print (jsonify(json.loads(model.to_json())))
-        # print("test-------------")
-
         # model_file =  "/home/user/ANS/QuiverTest/model.json"
-        # model_file = "/home/user/ANS/pytorch_model_vis/model_1.json"
         # with open(model_file, "r") as f:
         #     return jsonify(json.loads(f.read()))
-        return jsonify(graph)
+        return jsonify(_json_graph)
 
     @app.route('/inputs')
     def get_inputs():
-        return jsonify(list_img_files(input_folder))
+        print (list_img_files(_input_folder))
+        return jsonify(list_img_files(_input_folder))
 
     @app.route('/layer/<layer_name>/<input_path>')
     def get_layer_outputs(layer_name, input_path):
-        print (layer_name, input_path)
         
-        results = save_layer_outputs(model, hooks, graph, 
-                                    layer_name, input_folder,
-                                    input_path, temp_folder, use_gpu, image_size)
+        results = save_layer_outputs(_model, _hook_list, _json_graph, 
+                                    layer_name, _input_folder,
+                                    input_path, _temp_folder, _use_gpu, _image_size)
         return jsonify(results)
         
 
@@ -130,17 +106,64 @@ def get_app(model, hooks, classes, top, html_base_dir, use_gpu, image_size, temp
         results = [[("sa","bot_34", 0.2)],[("sa","bot_35", 0.6)]]
         return safe_jsonify(results)
 
-    return app
+
+def update_model(model, hooks, input_folder, image_size, use_gpu=False, temp_folder='./tmp'):
+    '''
+    update model
+    '''
+    global _json_graph, _model, _hook_list, _html_base_dir, _temp_folder, _input_folder, _use_gpu, _image_size
+    '''
+        prepare model
+    '''
+    width = 224
+    height = 224
+    if image_size is not None:
+        width = image_size[-1]
+        height = image_size[-2]
+    
+    _use_gpu = use_gpu
+
+    _image_size = [width, height]
+
+    x = torch.zeros(1, 3, width, height, dtype=torch.float, requires_grad=False)
+    
+    if _use_gpu and torch.cuda.is_available():
+        x = x.cuda()
+        _model = model.cuda()
+
+    _model = model
+
+    out = _model(x)
+    _json_graph = make_dot(out, params = dict(model.named_parameters()))
+
+    _input_folder = input_folder
+
+    _hook_list = hooks
+
+    _temp_folder = temp_folder
 
 
-def run_app(app, port=5000):
-    http_server = WSGIServer(('', port), app)
-    # webbrowser.open_new('http://localhost:' + str(port)) #
-    http_server.serve_forever()
+def register_model(model, hooks, use_gpu, image_size, temp_folder='./tmp', input_folder='./',
+            mean=None, std=None):
+    '''
+    :param model: the model to show
+    :param classes: list of names of output classes to show in the GUI.
+        if None passed - ImageNet classes will be used
+    :param top: number of top predictions to show in the GUI
+    :param html_base_dir: the directory for the HTML (usually inside the
+        packages, quiverboard/dist must be a subdirectory)
+    :param temp_folder: where the temporary image data should be saved
+    :param input_folder: the image directory for the raw data
+    '''
+    
+    update_model(model, hooks, input_folder, image_size, use_gpu=use_gpu, temp_folder=temp_folder)
+
+    register_routes()
 
 
-def launch(model, hooks, input_folder='./', use_gpu=False, image_size=None, classes=None, top=5, temp_folder='./tmp', 
-           port=5000, html_base_dir=None, mean=None, std=None):
+
+def launch(model, hooks, input_folder='./', use_gpu=False, image_size=None, temp_folder='./tmp', 
+           port=5000, html_base_dir=None):
     if platform.system() is 'Windows':
         temp_folder = '.\\tmp'
         os.system('mkdir %s' % temp_folder)
@@ -149,18 +172,18 @@ def launch(model, hooks, input_folder='./', use_gpu=False, image_size=None, clas
 
 
     html_base_dir = html_base_dir if html_base_dir is not None else dirname(abspath(__file__))
-    # print(html_base_dir)
     validate_launch(html_base_dir)
 
-    return run_app(
-        get_app(
-            model, hooks, classes, top,
-            html_base_dir=html_base_dir,
+    global _html_base_dir
+    _html_base_dir = html_base_dir
+
+    register_model(model, hooks,
             use_gpu=use_gpu,
             image_size=image_size,
             temp_folder=temp_folder,
-            input_folder=input_folder,
-            mean=mean, std=std
-        ),
-        port
-    )
+            input_folder=input_folder
+        )
+    
+    http_server = WSGIServer(('', port), app)
+    # webbrowser.open_new('http://localhost:' + str(port)) #
+    http_server.serve_forever()
